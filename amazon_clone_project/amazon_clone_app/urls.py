f73e440781360ee230733bc05d759182574f9ee8
anupom-1 <aa.anupom@gmail.com>
2026-04-15 21:35:03 +0600

    Day-4 feat(auth): add login and logout views

--- amazon_clone_project/amazon_clone_app/urls.py
+++ amazon_clone_project/amazon_clone_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.DashBoardView.as_view(), name="dashboard"),
-    path("register/", views.RegisterView.as_view(), name="register")
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", views.LogInView.as_view(), name="login"),
+    path("logout/", views.LogOutView.as_view(), name="logout")
 ]
--- amazon_clone_project/amazon_clone_app/views.py
+++ amazon_clone_project/amazon_clone_app/views.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView, ListView, DetailView, DeleteView, UpdateView, CreateView
+from django.views.generic import TemplateView, ListView, DetailView, DeleteView, UpdateView, CreateView, FormView
 from django.shortcuts import redirect
 from . import models
+from django.views import View
+from django.contrib.auth import authenticate,logout, login
+from django.urls import reverse
+from django.shortcuts import redirect
+from . import forms
 
 # DashBoardView
 class DashBoardView(TemplateView):
@@ -10,10 +15,10 @@ class DashBoardView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
-        if models.CustomUser.objects.get(id = user.id):
+        if models.CustomUser.objects.filter(id = user.id).exists():
             context["user"] = f"Welcome {user.first_name}"
             return context
-        return None
+        return context
     
 # RegisterView
 class RegisterView(CreateView):
@@ -31,3 +36,29 @@ class RegisterView(CreateView):
         models.Profile.objects.create(user=user)
         self.object = user
         return redirect(self.success_url) 
+    
+class LogInView(FormView):
+    template_name = "amazon_clone_app/login.html"
+    form_class = forms.LoginForm
+    # initial = None
+    # success_url = "dashboard"
+
+    def form_valid(self, form):
+        email = form.cleaned_data["email"]
+        password = self.request.POST.get("password")
+        user = authenticate(self.request, email=email, password=password)
+        if user is not None:
+            login(self.request, user)
+            models.Profile.objects.get_or_create(user=user)
+            return redirect("dashboard")
+        form.add_error(None, "invalid email or password")
+        return self.form_invalid(form)
+
+# LogOutView   
+class LogOutView(View):
+
+    def post(self, request):
+        logout(request)
+        return redirect("login")
+
+
